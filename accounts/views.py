81cf5cf9bf94f9213ec460f9557e093809e03459
D-Lesev <danaillesev@gmail.com>
2023-07-15 00:09:33 +0300

    html and css refactoring

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
@@ -7,3 +8,10 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        return self.request.GET.get('next')
